56ce1c514dcb684ce35ea662620cfc2260f3a444
Nitipat Thanaborvornwiwat <th.nitipat@gmail.com>
2020-03-01 02:08:47 +0700

    Add test getMovieId

--- src/test/java/com/nitipat/javaspring/guava/GuavaApplicationTests.java
+++ src/test/java/com/nitipat/javaspring/guava/GuavaApplicationTests.java
@@ -1,13 +1,34 @@
 package com.nitipat.javaspring.guava;
 
+import java.util.concurrent.ExecutionException;
+
+import org.hamcrest.Matchers;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.nitipat.javaspring.guava.domain.dto.MovieDto;
+import com.nitipat.javaspring.guava.service.MovieService;
 
 @SpringBootTest
+@RunWith(SpringJUnit4ClassRunner.class)
 class GuavaApplicationTests {
-
+	
+	@Autowired
+	private MovieService movieService;
+	
 	@Test
-	void contextLoads() {
+	public void testgetMovieById() throws ExecutionException {
+		MovieDto result = movieService.getMovieById(1L);
+		Assert.assertEquals(result.getTitle(), "Destroyer");
+		Assert.assertThat(result.getYear(),  Matchers.equalTo(2018L));
+		
+		MovieDto resultFromCache = movieService.getMovieById(1L);
+		Assert.assertEquals(resultFromCache.getTitle(), "Destroyer");
+		Assert.assertThat(resultFromCache.getYear(),  Matchers.equalTo(2018L));
 	}
 
 }
