958ee561ec82b01d148e3210517da7638cfbdb1c
Nitipat Thanaborvornwiwat <th.nitipat@gmail.com>
2020-03-01 14:23:35 +0700

    modified testHitCache

--- src/test/java/com/nitipat/javaspring/guava/GuavaApplicationTests.java
+++ src/test/java/com/nitipat/javaspring/guava/GuavaApplicationTests.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.nitipat.javaspring.guava.configuration.MovieConfiguration;
 import com.nitipat.javaspring.guava.domain.dto.MovieDto;
 import com.nitipat.javaspring.guava.service.MovieService;
 
@@ -20,15 +21,23 @@ class GuavaApplicationTests {
 	@Autowired
 	private MovieService movieService;
 	
+	@Autowired
+	private MovieConfiguration movieConfiguration;
+	
 	@Test
-	public void testgetMovieById() throws ExecutionException {
+	public void testHitCache() throws ExecutionException {
+		// try to get data first time 
+		Assert.assertThat(movieConfiguration.getCacheStats().hitCount(), Matchers.equalTo(0L));
 		MovieDto result = movieService.getMovieById(1L);
 		Assert.assertEquals(result.getTitle(), "Destroyer");
 		Assert.assertThat(result.getYear(),  Matchers.equalTo(2018L));
 		
+		// hit cache
+		Assert.assertThat(movieConfiguration.getCacheStats().hitCount(), Matchers.equalTo(1L));
 		MovieDto resultFromCache = movieService.getMovieById(1L);
 		Assert.assertEquals(resultFromCache.getTitle(), "Destroyer");
 		Assert.assertThat(resultFromCache.getYear(),  Matchers.equalTo(2018L));
+		
 	}
 
 }
